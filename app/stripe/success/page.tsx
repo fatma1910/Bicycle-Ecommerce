4cc4966c95c173dd165d7194ff23b0479a1265db
unknown <fatmasaleh567@gmail.com>
2024-09-30 07:33:54 +0300

    first commit

--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,18 +1,31 @@
-import Link from 'next/link'
+'use client';
+import { useEffect } from 'react';
+import { useShoppingCart } from 'use-shopping-cart';
+import Link from 'next/link';
 
 const SuccessPage = () => {
+  const { clearCart } = useShoppingCart();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      clearCart(); // Clear the cart only on the client side
+    }
+  }, [clearCart]);
+
   return (
     <section className='py-72'>
       <div className='container mx-auto'>
         <h3 className='text-center mb-4'>
-          Your payment wasw successful! Thank you.
+          Your payment was successful! Thank you.
         </h3>
         <Link href='/'>
-          <button className='btn btn-primary mx-auto'>Back to the home page</button>
+          <button className='btn btn-primary mx-auto'>
+            Back to the home page
+          </button>
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SuccessPage
+export default SuccessPage;
